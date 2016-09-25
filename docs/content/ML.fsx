a0cb1ab7d582fe0d5eceb6fecb714a90b84966f9
Timo Mühlhaus <muehlhaus@bio.uni-kl.de>
2016-09-25 16:59:39 +0200

    Init docs

---
+++ docs/content/ML.fsx
@@ -0,0 +1,18 @@
+(*** hide ***)
+// This block of code is omitted in the generated HTML documentation. Use 
+// it to define helpers that you do not want to show in the documentation.
+#I "../../bin"
+
+(**
+Machine learning
+================
+Machine learning is a subfield of computer science that evolved from the study of pattern recognition and computational learning theory in artificial intelligence. In 1959, Arthur Samuel defined machine learning as a "Field of study that gives computers the ability to learn without being explicitly programmed".
+
+*)
+#r "BioFSharp.Stats.dll"
+open BioFSharp.Stats
+
+Library.hello 0
+(**
+Some more info
+*)
---
+++ docs/content/Testing.fsx
@@ -0,0 +1,19 @@
+(*** hide ***)
+// This block of code is omitted in the generated HTML documentation. Use 
+// it to define helpers that you do not want to show in the documentation.
+#I "../../bin"
+
+(**
+Hypothesis test
+===============
+
+A hypothesis test is a statistical test that is used to determine whether there is enough evidence in a sample of data to infer that a certain condition is true for the entire population. A hypothesis test examines two opposing hypotheses about a population: the null hypothesis and the alternative hypothesis.
+
+*)
+#r "BioFSharp.Stats.dll"
+open BioFSharp.Stats
+
+Library.hello 0
+(**
+Some more info
+*)
